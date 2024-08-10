d6bd5e449c160f7a231b861c4b2601a2f3ca319b
dev-m-josh <168928089+dev-m-josh@users.noreply.github.com>
2024-08-10 13:10:56 +0300

    width responsiveness

--- calculator.js
+++ calculator.js
@@ -3,43 +3,12 @@ let display = document.querySelector(".screen");
 let buttons = document.querySelectorAll(".btn");
 let clear = document.querySelector(".btn-clear");
 let equals = document.getElementById("equals");
-/*
-//display the clicked number
-function addToDisplay(input) {
-    display.value += input;
-}
 
-
-//equals button
-equals.addEventListener('click', getAnswer)
-//get the answer after equal button is clicked
-function getAnswer(){
-    try {
-        if (display.value === '') {
-            display.value = "";
-        } else {
-            //use eval() to evaluate the calculations
-            display.value= eval(display.value);
-        }
-        //catch any error
-    } catch (error) {
-        display.value = "Error";
-    }
-
-}
-
-//clear the display to add another calculation
-clear.addEventListener('click', clearData);
-//when the button is clicked it clears the display
-function clearData() {
-    display.value = "";
-}
-*/
 function addToDisplay() {
     
 }
 
-//Method 2
+
 //display the clicked numbers and operators
 buttons.forEach(function (button) {
     button.addEventListener('click', function (e) {
